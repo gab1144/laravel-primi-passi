3e6b4b5d74bfb2eff02aef203d36087ff3e72c13
gab1144 <gabry.rinciari04@gmail.com>
2022-12-21 17:36:52 +0100

    [add] style css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Ciao Laravel</title>
 </head>
 <body>
-    <h1>Hello world</h1>
-    <h2>{{ $saluto }} {{ $utente }}!</h2>
+    <div class="container">
+        <h1>Hello world</h1>
+        <h2>{{ $saluto }} {{ $utente }}!</h2>
+    </div>
 </body>
 </html>
